bc2b3509f7f8214638c32f01b823a37ac1965594
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2016-10-07 10:42:27 -1000

    added put request

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const qs = require ('querystring');
 const PORT = 3000;
 
 http.createServer((request, response) => {
-
+console.log(request.url);
   if(request.method === 'GET'){
 
     let pathName = request.url;
@@ -148,12 +148,81 @@ http.createServer((request, response) => {
 
     });
 
-
-
-
   }
 
   if(request.method === 'PUT'){
+
+    let fileToAlter;
+    request.on('data', (data)=>{
+
+      fileToAlter = data.toString();
+
+    });
+
+    request.on('end',()=>{
+
+      const parsedFile = qs.parse(fileToAlter);
+      const fileName = './public/'+parsedFile.elementName.toLowerCase() + '.html';
+
+      let file=
+      `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - ${parsedFile.elementName}</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>${parsedFile.elementName}</h1>
+  <h2>${parsedFile.elementSymbol}</h2>
+  <h3>Atomic number ${parsedFile.elementAtomicNumber}</h3>
+  <p>${parsedFile.elementDescription}.</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>
+`;
+
+      request.url = fileName;
+
+      fs.readFile(request.url, (err, data)=>{
+        if(err){
+
+
+        let statusCode = 500;
+
+
+          response.writeHead(statusCode, {
+            'Content-Type': 'application/json',
+          });
+
+          response.end(JSON.stringify({'error' : 'resource' + fileName + ' does not exist' }));
+
+
+
+      } else {
+        fs.writeFile(fileName, file, (err)=>{
+          if (err){
+            response.end(err.message);
+
+          }else{
+            let statusCode = 200;
+
+            response.writeHead(statusCode, {
+              'Content-Type': 'application/json',
+            });
+            response.end(JSON.stringify({'success':true}));
+
+          }
+
+        });
+
+      }
+
+    });
+
+  });
+
+
 
   }
 
